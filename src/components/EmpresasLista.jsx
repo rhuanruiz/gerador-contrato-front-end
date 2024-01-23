83c03e74bb642e3c85f3cb6a6bc4b12edc205928
rhuanruiz <rhuanruiz@hotmail.com>
2024-01-23 11:58:02 -0300

    Componente de listagem

--- src/components/EmpresasLista.jsx
+++ src/components/EmpresasLista.jsx
@@ -1,10 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { 
+  useState, 
+  useEffect 
+} from 'react';
+import { 
+  Table, 
+  TableBody, 
+  TableCell, 
+  TableContainer, 
+  TableHead, 
+  TableRow, 
+  Box, 
+  Button, 
+  Paper
+} from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CriarEmpresa from './CriarEmpresa';
 import axios from 'axios';
 
 
 export default function EmpresasLista() {
 
     const [empresas, setEmpresas] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         const fetchEmpresas = async () => {
@@ -18,26 +35,54 @@ export default function EmpresasLista() {
         fetchEmpresas();
     }, []);
 
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+    
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
+
+    const handleDelete = async (empresa) => {
+        try {
+            const response = await axios.delete(`${process.env.REACT_APP_API_URL}/empresa/${empresa.id}`);
+            console.log(response.data);
+            setTimeout(() => {
+              //this.msg_sucesso = '';
+              window.location.reload(true);
+            }, 2000);
+        } catch (error) {
+            console.error('Ocorreu um erro, tente novamente.\nErro:', error);
+        }
+    };
 
     return (
-        <div>
-          <h2>Empresas Cadastradas</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>CNPJ</th>
-              </tr>
-            </thead>
-            <tbody>
-              {empresas.map((empresa) => (
-                <tr key={empresa.id}>
-                  <td>{empresa.nome}</td>
-                  <td>{empresa.cnpj}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
+        <Box>
+            <Button variant="contained" startIcon={<AddCircleIcon />} onClick={handleOpenModal}>Nova Empresa</Button>
+            <CriarEmpresa open={openModal} onClose={handleCloseModal} />
+                <Box sx={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <TableContainer component={ Paper }>
+                        <Table sx={{ minWidth: 650 }} aria-label="empresas">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">Empresas</TableCell>
+                                    <TableCell align="center">CNPJ</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {empresas.map((empresa) => (
+                                    <TableRow key={ empresa.id }>
+                                        <TableCell component="th" scope="row">{ empresa.nome }</TableCell>
+                                        <TableCell>{empresa.cnpj}</TableCell>
+                                        <TableCell>
+                                            <Button variant="outlined" onClick={() => handleDelete(empresa)}>Excluir</Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>  
+                    </TableContainer>
+                </Box>
+        </Box>
+    );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,19 +1,34 @@
-// LandingPage.js
 import React from 'react'
 import EmpresasLista from './EmpresasLista';
-import '../styles/LandingPage.css'
+import { 
+  Box, 
+  Button
+} from '@mui/material';
+import ArticleIcon from '@mui/icons-material/Article';
+
+
+const style = {
+    backgroundSize: 'cover',
+    backgroundAttachment: 'fixed',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(31, 30, 30)'
+};
 
 const LandingPage = ({ empresas, representantes }) => {
-  const handleGerarDocumento = () => {
-    // Lógica para gerar o documento
-  };
+    const handleGerarDocumento = () => {
+        // Lógica para gerar o documento
+    };
 
-  return (
-    <div className='landing-page'>
-      <button onClick={handleGerarDocumento}>Gerar Documento</button>
-      <EmpresasLista empresas={empresas} />
-    </div>
-  );
+    return (
+        <Box sx={style}>
+            <Button variant="contained" size="large" style={{ marginBottom: '15vh' }} startIcon={<ArticleIcon />}>Gerar Documento</Button>
+            <EmpresasLista empresas={empresas} />
+        </Box>
+    );
 };
 
 export default LandingPage;
